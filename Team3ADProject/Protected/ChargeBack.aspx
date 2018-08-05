a9799bd0d92c2970f7e2e35e574d51fae63f3bc1
Rohit <rohitkr412@gmail.com>
2018-08-05 17:55:38 +0800

    Adding ChargeBack page

---
+++ Team3ADProject/Protected/ChargeBack.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="ChargeBack.aspx.cs" Inherits="Team3ADProject.Protected.ChargeBack" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Chargeback</h1>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="From: "></asp:Label>
+                    <asp:TextBox ID="TextBox1" runat="server" TextMode="Date"></asp:TextBox>
+                 
+                </td>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="To: "></asp:Label>
+                    <asp:TextBox ID="TextBox2" runat="server" TextMode="Date"></asp:TextBox>
+                      </td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Search" UseSubmitBehavior="false" OnClick="Button1_Click" CssClass="btn btn-success"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CssClass="table table-hover">
+        <Columns>
+            <asp:BoundField DataField="description" HeaderText="Item" />
+            <asp:BoundField DataField="received_quantity" HeaderText="Qty Received" />
+            <asp:BoundField DataField="price" HeaderText="Cost" DataFormatString="{0:c2}" />
+        </Columns>
+    </asp:GridView>
+    </div>
+    <asp:Label ID="Label3" runat="server" Text="Total Price: "></asp:Label>
+    <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+    <div>
+    <asp:Button ID="Button2" runat="server" Text="Send ChargeBack" OnClick="Button2_Click" CssClass="btn btn-success" />
+    </div>
+
+
+</asp:Content>
---
+++ Team3ADProject/Protected/ChargeBack.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Team3ADProject.Code;
+using Team3ADProject.Model;
+using System.Globalization;
+
+namespace Team3ADProject.Protected
+{
+    public partial class ChargeBack : System.Web.UI.Page
+    {
+        private static List<deptusagechargeback_Result> list;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            if (!IsPostBack)
+            {
+                List<department> depts = new List<department>();
+                depts = BusinessLogic.ReturnDep();
+                DropDownList1.DataSource = depts;
+                DropDownList1.DataValueField = "department_id";
+                DropDownList1.DataTextField = "department_name";
+                DropDownList1.DataBind();
+                DropDownList1.SelectedIndex = 0;
+                string month = DateTime.Today.Month.ToString();
+                if (month.Count() == 1)
+                {
+                    month = "0" + month;
+                }
+                string year = DateTime.Today.Year.ToString();
+                TextBox1.Text = (year+"-"+month+"-"+"01");
+                TextBox2.Text = DateTime.Today.ToString("yyyy-MM-dd");
+                list = new List<deptusagechargeback_Result>();
+                Label4.Visible = false;
+                Label3.Visible = false;
+                Button2.Visible = false;
+            }
+        }
+
+        protected void loadGrid()
+        {
+           try
+            {
+
+                DateTime startdate;
+                DateTime enddate;
+                string dept = DropDownList1.SelectedItem.Value;
+                startdate = DateTime.ParseExact(TextBox1.Text, "yyyy-MM-dd", null);
+                enddate = DateTime.ParseExact(TextBox2.Text, "yyyy-MM-dd", null);
+                list = BusinessLogic.UsageChargeBack(startdate, enddate, dept.Trim());
+                GridView1.DataSource = list;
+                GridView1.DataBind();
+                if (list.Count > 0)
+                {
+                    double price = list.Sum(x => x.price).Value;
+                    Label4.Text = String.Format("{0:c2}", price);
+                    Label3.Text = "Total price";
+                    Label4.Visible = true;
+                    Label3.Visible = true;
+                    Button2.Enabled = true;
+                    Button2.Visible = true;
+                }
+                else
+                {
+                    Button2.Enabled = false;
+                    Label3.Visible = true;
+                    Label3.Text = "Selected criteria have no result";
+                    Label4.Visible = false;
+                    Button2.Visible = false;
+                }
+            }
+
+            catch(Exception ex)
+            {
+                Label3.Text = "Please enter a valid date.";
+                Label3.Visible = true;
+            }
+            
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            loadGrid();   
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            DateTime startdate, enddate;
+            startdate = DateTime.ParseExact(TextBox1.Text, "yyyy-MM-dd", null);
+            enddate = DateTime.ParseExact(TextBox2.Text, "yyyy-MM-dd", null);
+            string dept = DropDownList1.SelectedItem.Value.Trim();
+            int dephead = BusinessLogic.departmentheadidbydeptid(dept);
+            string to = BusinessLogic.RetrieveEmailByEmployeeID(dephead);
+            string sub = "Stationery System: Usage Charge Back";
+            string body = "Usage charge back for " + DropDownList1.SelectedItem.ToString() + " from "+ startdate.ToString("dd-MM-yyyy")+ " to "+ enddate.ToString("dd-MM-yyyy") + "is $ " + list.Sum(x=>x.price);
+            BusinessLogic.sendMail(to, sub, body);
+        }
+    }
+}
